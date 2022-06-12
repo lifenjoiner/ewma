47972ad164a91193e79d7b57ca02d0d33d8ccd32
YX Hao <lifenjoiner@163.com>
2022-06-12 15:07:17 +0800

    More tests

--- ewma_test.go
+++ ewma_test.go
@@ -42,13 +42,23 @@ func TestEWMA(t *testing.T) {
 	}
 	// Preset samples, Value()
 	var tests = [20]float64 {334, 373, 82, 141, 38, 443, 442, 397, 55, 60, 377, 401, 204, 419, 51, 217, 129, 238, 150, 341}
+	// Init with Set.
 	var ewmas = [20]float64 {334, 341, 294, 266, 225, 264, 297, 315, 268, 230, 257, 283, 269, 296, 251, 245, 224, 227, 213, 236}
 	ewma = NewMovingAverage(10)
 	ewma.Set(tests[0])
 	for i := 0; i < len(tests); i++ {
-	    ewma.Add(tests[i])
-	    if math.Round(ewma.Value()) != ewmas[i] {
-	        t.Errorf("%v: value = %v, expected = %v", i, ewma.Value(), ewmas[i])
-	    }
+		ewma.Add(tests[i])
+		if math.Round(ewma.Value()) != ewmas[i] {
+			t.Errorf("%v: value = %v, expected = %v", i, ewma.Value(), ewmas[i])
+		}
+	}
+	// Init with Add
+	ewmas = [20]float64 {334, 360, 221, 189, 139, 226, 280, 306, 256, 220, 249, 276, 263, 291, 248, 242, 222, 225, 211, 235}
+	ewma = NewMovingAverage(10)
+	for i := 0; i < len(tests); i++ {
+		ewma.Add(tests[i])
+		if math.Round(ewma.Value()) != ewmas[i] {
+			t.Errorf("%v: value = %v, expected = %v", i, ewma.Value(), ewmas[i])
+		}
 	}
 }
